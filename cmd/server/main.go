d81f87fb88570325a2ee3dbc5079356a128dbf61
koraytoprak <89402985+koraytoprak@users.noreply.github.com>
2021-10-16 10:55:00 +0300

    first build

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,95 @@
-// Package main is the entry point to the server. It reads configuration, sets up logging and error handling,
-// handles signals from the OS, and starts and stops the server.
 package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
+	"go.uber.org/zap"
 )
 
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+host := getStringOrDefault("HOST", "localhost")
+	port := getIntOrDefault("PORT", 8080)
+
+	s := server.New(server.Options{
+		Host: host,
+		Port: port,
+	})
+
+	var eg errgroup.Group
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	eg.Go(func() error {
+		<-ctx.Done()
+		if err := s.Stop(); err != nil {
+			log.Info("Error stopping server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	if err := s.Start(); err != nil {
+		log.Info("Error starting server", zap.Error(err))
+		return 1
+	}
+
+	if err := eg.Wait(); err != nil {
+		return 1
+	}
+
+func start() int {
+	s := server.New(server.Options{
+		Host: host,
+		Log: log,
+		Port: port,
+	})
+	logEnv := getStringOrDefault("LOG_ENV", "development")
+	log, err := createLogger(logEnv)
+	if err != nil {
+		fmt.Println("Error setting up the logger:", err)
+		return 1
+	}
+	defer func() {
+		
+		
+		_ = log.Sync()
+	}()
+
+	return 0
+}
+
+func createLogger(env string) (*zap.Logger, error) {
+	switch env {
+	case "production":
+		return zap.NewProduction()
+	case "development":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+}
+
+func getStringOrDefault(name, defaultV string) string {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	return v
+}
+
+func getIntOrDefault(name string, defaultV int) int {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	vAsInt, err := strconv.Atoi(v)
+	if err != nil {
+		return defaultV
+	}
+	return vAsInt
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module canvas
 
 go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.4
+	github.com/matryer/is v1.4.0
+	go.uber.org/atomic v1.9.0
+	go.uber.org/multierr v1.7.0
+	go.uber.org/zap v1.19.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
